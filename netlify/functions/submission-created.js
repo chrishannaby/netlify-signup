ce90a121dec296e875060b184813af6db3013260
Chris Hannaby <chris.hannaby@netlify.com>
2021-10-06 16:36:48 -0600

    add form handling

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,7 +1,29 @@
 const fetch = require("node-fetch");
 
-const { NETLIFY_ACCESS_TOKEN } = process.env;
+const { NETLIFY_ACCESS_TOKEN, NETLIFY_SLUG } = process.env;
 
-exports.handler = function (event, context, callback) {
-  console.log(NETLIFY_ACCESS_TOKEN);
+exports.handler = async function (event) {
+  const body = JSON.parse(event.body);
+  const { email } = body.payload.data;
+  const response = await fetch(
+    `https://api.netlify.com/api/v1/${NETLIFY_SLUG}/members?` +
+      new URLSearchParams({
+        email,
+        role: "Collaborator",
+      }),
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${NETLIFY_ACCESS_TOKEN}`,
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+  return {
+    statusCode: 301,
+    headers: {
+      Location: "/success",
+    },
+  };
 };
